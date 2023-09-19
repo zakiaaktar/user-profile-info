a0f879435da196b2fea212c4da23a7cf644c102e
Zakia Aktar <zakiaaktar@Zakias-MacBook-Pro.local>
2023-09-19 20:56:53 +0200

    Project and Basic Layout Setup

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Users from '../Users/Users';
+
+const Home = () => {
+    return (
+        <div>
+            <Users></Users>
+        </div>
+    );
+};
+
+export default Home;
