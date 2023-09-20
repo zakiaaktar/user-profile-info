916c401b8d9c66dcdfb057dc3f354923c1753d09
Zakia Aktar <zakiaaktar@Zakias-MacBook-Pro.local>
2023-09-20 10:30:42 +0200

    data fetch and display in ui

--- src/Pages/Users/UserCard.js
+++ src/Pages/Users/UserCard.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const UserCard = () => {
+const UserCard = ({user}) => {
+    const {id, name, email, img} = user;
     return (
         <div>
-            
+            <img src={img} alt="user-img" />
+            <h2>Name: {name}</h2>
+            <h3>Email: {email}</h3>
         </div>
     );
 };
--- src/Pages/Users/Users.js
+++ src/Pages/Users/Users.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import UserCard from './UserCard';
 
 const Users = () => {
+    const [users, setUsers] = useState([]);
+
+
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, []);
+
+
     return (
-        <div>
-            <h2>users</h2>
+        <div className='my-16'>
+        <div className='text-center'>
+        <h2 className="text-4xl font-serif font-bold text-blue-700"> USER PROFILE INFO</h2>
         </div>
+         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-8'>
+                {
+                    users.map(user => <UserCard
+                        key={user._id}
+                        user={user}
+                    ></UserCard>)
+                }
+            </div>
+            </div>
     );
 };
 
